436f96d5d3a39c82a470733b4a406e04cf7d5daf
Aditya Khera <adityakhera@Adityas-MacBook-Pro.local>
2024-07-09 20:39:55 +0100

    Added Make A Listing

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -4,3 +4,11 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class AuctionListings(models.Model):
+    listing_name = models.CharField(max_length=64)
+    listing_description = models.CharField(max_length=128)
+    listing_photo = models.ImageField(upload_to='listingimages', blank=True, default='listingimages/Image_not_available.png')
+    listing_starting = models.IntegerField()
+    listing_maker = models.ForeignKey(User, on_delete=models.CASCADE, related_name='listings_made')
+
