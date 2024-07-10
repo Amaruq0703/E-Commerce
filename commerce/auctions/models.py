f83d026f6b567976f00615cb06fdafbdf7a01f55
Amaruq0703 <aditya.khera7@gmail.com>
2024-07-10 04:17:27 +0100

    Shown Active Listings on Home Page

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -6,9 +6,30 @@ class User(AbstractUser):
     pass
 
 class AuctionListings(models.Model):
+    VEHICLES = "VEH"
+    FASHINON = "FAS"
+    DEVICES = "DEV"
+    TOOLS = "TOO"
+    SPORTS = "SPO"
+    TOYS = "TOY"
+    ENTERTAINMENT = "ENT"
+    MISCELLANIOUS = "MIS"
+
+    CATEGORY = [
+        (VEHICLES, "Vehicles"),
+        (FASHINON, "Fashion"),
+        (DEVICES, "Devices"),
+        (TOOLS, "Tools"),
+        (SPORTS, "Sports"),
+        (TOYS, "Toys"),
+        (ENTERTAINMENT, "Entertainment"),
+        (MISCELLANIOUS, "Miscellanious")
+    ]
+    
     listing_name = models.CharField(max_length=64)
     listing_description = models.CharField(max_length=128)
-    listing_photo = models.ImageField(upload_to='listingimages', blank=True, default='listingimages/Image_not_available.png')
+    listing_photo = models.URLField(blank=True)
     listing_starting = models.IntegerField()
     listing_maker = models.ForeignKey(User, on_delete=models.CASCADE, related_name='listings_made')
+    listing_category = models.CharField(max_length=3, choices=CATEGORY)
 
