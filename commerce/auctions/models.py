6243e86d1d2748c05efc8d985fb9f4e9184ed9a3
Aditya Khera <adityakhera@Adityas-MacBook-Pro.local>
2024-07-13 05:42:20 +0100

    Added Bidding Functionality

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-
+from django.core.exceptions import ValidationError
 
 class User(AbstractUser):
     pass
@@ -32,6 +32,7 @@ class AuctionListings(models.Model):
     listing_starting = models.IntegerField()
     listing_maker = models.ForeignKey(User, on_delete=models.CASCADE, related_name='listings_made')
     listing_category = models.CharField(max_length=3, choices=CATEGORY)
+    listing_status = models.BooleanField(default=True)
 
 class Comment(models.Model):
     comment_text = models.CharField(max_length=400)
@@ -42,3 +43,15 @@ class Watchlist(models.Model):
     watchlist_listing = models.ForeignKey(AuctionListings, on_delete=models.CASCADE, related_name='in_watchlists')
     watchlist_maker = models.ForeignKey(User, on_delete=models.CASCADE, related_name='watchlist_items')
 
+class Bid(models.Model):
+    bid_maker = models.ForeignKey(User, on_delete=models.CASCADE, related_name='bids_made')
+    bid_item = models.ForeignKey(AuctionListings, on_delete=models.CASCADE, related_name='bids')
+    bid_amount = models.IntegerField()
+
+    def clean(self):
+        if self.bid_amount < self.bid_item.listing_starting:
+            raise ValidationError(f'Bid amount cannot be less than the starting price of {self.bid_item.listing_starting}.')
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        super().save(*args, **kwargs)
